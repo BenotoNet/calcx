d723b0e62e0ed3050eabb5d2435d6a4e22a1d1e3
bendix <bendix@benoto.net>
2026-06-24 07:52:09 +0200

    Added more units (time + some physics like joule)

--- src/calc/tokenize/misc_units.rs
+++ src/calc/tokenize/misc_units.rs
@@ -1,6 +1,8 @@
 use crate::calc::num::Num;
 pub fn unit_to_num(ul: &str) -> Option<Num> {
     Some(match ul {
+
+        // Powers of Ten
         "exa" => Num::unitless(10.0f64.powf(18.0)),
         "peta" => Num::unitless(10.0f64.powf(15.0)),
         "tera" => Num::unitless(10.0f64.powf(12.0)),
@@ -15,6 +17,21 @@ pub fn unit_to_num(ul: &str) -> Option<Num> {
         "micro" => Num::unitless(10.0f64.powf(-6.0)),
         "nano" => Num::unitless(10.0f64.powf(-9.0)),
         "pico" => Num::unitless(10.0f64.powf(-12.0)),
+
+        // Time Units
+        "minute"|"minutes" => {Num::new(60.0, vec![('s', 1)])}
+        "hour"|"hours" => {Num::new(3600.0, vec![('s', 1)])}
+        "day"|"days" => {Num::new(86400.0, vec![('s', 1)])}
+        "week"|"weeks" => {Num::new(604800.0, vec![('s', 1)])}
+        "year"|"years" => {Num::new(31557600.0, vec![('s', 1)])}
+
+        // Computers
+        "byte"|"bytes" => {Num::unitless(8.0)}
+
+        // Physics
+        "joule"|"joules" => {Num::new(1000.0, vec![('g', 1), ('m', 2), ('s', -2)])}
+        "watt"|"watts" => {Num::new(1000.0, vec![('g', 1), ('m', 2), ('s', -3)])}
+
         _ => return None
     })
 }
