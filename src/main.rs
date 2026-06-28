2230755fe34750513f9c19f8834762d654c5ef48
Bendix <bendix@benoto.net>
2026-06-28 20:51:04 +0200

    Testing with rug::Float (-> replacement for f64)

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,19 @@ mod calc;
 mod utils;
 
 use calc::num::Num;
+use rug::Float;
 
 use std::env::args;
 use ui::Option;
+
+const PRECISION: u32 = 4096;
 
 fn main() {
     let args = args().collect::<Vec<String>>();
 
     let options = utils::parse_cli_args(args);
+
+    println!{"{}", Float::with_val(PRECISION, 1.02)}
 
     // All options are set, therefore, we can start the interactive session with the options
     ui::UI::new(options).interactive();
